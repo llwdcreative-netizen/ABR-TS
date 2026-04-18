b6a1854890fc5bd863eef954695fe91cd107c275
LLWD <llwd.creative@gmail.com>
2026-04-18 13:13:51 -0300

    add init_db()

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import os
+from backend.db import init_db
 
-# 👇 CARGAR .env PRIMERO
 load_dotenv()
 
 from backend import create_app
 
 app = create_app()
+
+init_db()
 
 UPLOAD_FOLDER = os.path.join("static", "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
