dc4fab1bdefd1a5695b1b96ad91eab1516cddf79
LLWD <llwd.creative@gmail.com>
2026-04-22 19:43:52 -0300

    corrección de reset_db.py

--- reset_db.py
+++ reset_db.py
@@ -1,4 +1,17 @@
 from backend.db import get_db
+from dotenv import load_dotenv
+import os
+
+# Cargar variables del .env
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
+
+if not db_url:
+    raise RuntimeError("DATABASE_URL no está configurada")
+
+print("🔥 Usando DB:", db_url)
+print("🧨 Borrando datos...")
 
 with get_db() as db:
     with db.cursor() as cur:
